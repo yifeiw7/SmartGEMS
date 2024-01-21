120528f1ac1c2f116bc8f4a06fc56f413369ee49
Yijie Wang <147119743+yijie-04@users.noreply.github.com>
2024-01-21 12:11:15 -0500

    Update arduino_to_py.py

--- interface/arduino_to_py.py
+++ interface/arduino_to_py.py
@@ -1,41 +1,29 @@
 import serial
 import time
 
-def extract_numbers(input_string, delimiter=" "):
-    numbers = [int(num) for num in input_string.split(delimiter) if num.isdigit()]
-    if numbers != []:
-        return numbers
     
 def read():
-
 	ser = serial.Serial(
-		port='/dev/cu.usbmodem101',
+		port='/dev/cu.usbmodem1101',
 		baudrate=9600,
 		parity=serial.PARITY_NONE,
 		stopbits=serial.STOPBITS_ONE,
 		bytesize=serial.EIGHTBITS,
-		timeout=0.1
 	)
 
-	numeric_data_list = []
-	count = 1
 
-	while True:
-		data = ser.readline().decode('utf-8').strip()
-		if data:
-			values = data.split(',')
-			for value in values:
-				k = extract_numbers(value)
-				if k != None:
-					numeric_data_list.append(k[0])
-			count += 1
+	data = ser.readline().decode('utf-8').strip()
+	data = data.split(',')
+	for i in range(len(data)):
+		if data[i] != '':
+			data[i] = int(data[i])
 
-		if count >= 5:  # Change 10 to the desired number of readings
-			break
-		time.sleep(0.03)
+	print(data)
 
-	return [{'Temperature': numeric_data_list[4],
-		   	 'Water_Height': numeric_data_list[1],
-			 'Moisture_Level': numeric_data_list[2],
-			 'Wind': numeric_data_list[5],
-			 'Light_In': numeric_data_list[0]}]
+	return [{'Temperature': data[0],
+		   	 'Water_Height': data[1],
+			 'Moisture_Level': data[2],
+			 'Wind': data[3],
+			 'Light_In': data[4]}]
+
+read()
